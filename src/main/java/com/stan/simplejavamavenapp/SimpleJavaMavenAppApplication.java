cf7b018fb3f066cbab12d27907cb0f4bb2f668eb
Stan <1576302867@qq.com>
2019-12-10 19:41:39 +0800

    initial project

---
+++ src/main/java/com/stan/simplejavamavenapp/SimpleJavaMavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.stan.simplejavamavenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJavaMavenAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJavaMavenAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stan/simplejavamavenapp/SimpleJavaMavenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stan.simplejavamavenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaMavenAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
