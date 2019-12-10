69d2cf2af005ad15e37ce4219492f99e5e5601f7
Stan <1576302867@qq.com>
2019-12-10 19:45:21 +0800

    添加控制器

---
+++ src/main/java/com/stan/simplejavamavenapp/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.stan.simplejavamavenapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MainController {
+
+    @GetMapping("index")
+    public String index() {
+        return "Index";
+    }
+}
